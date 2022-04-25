8c7d9b73b55e9b4d480023fe33b2187f5478ed3f
Alexander Zhibrailov <62484582+ZhiAlex@users.noreply.github.com>
2022-04-26 02:11:29 +0300

    Update DeleteUserTest.cs

--- ReqresInAutotests/Autotests/Tests/DeleteUserTest.cs
+++ ReqresInAutotests/Autotests/Tests/DeleteUserTest.cs
@@ -14,8 +14,7 @@ public class DeleteUserTest : BaseTest
     {
         int id = 2;
 
-        var createUser = new RestRequest("/api/users/" + id, Method.Delete)
-            .AddHeader("Content-Type", ContentType.Json);
+        var createUser = new RestRequest("/api/users/" + id, Method.Delete);
         var response = await Client.DeleteAsync(createUser);
 
         Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
